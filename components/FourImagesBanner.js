edee9e0336b9cd782695935b86ab63dc90353bb5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-20 11:07:28 +0200

    Update FourImagesBanner.js

--- components/FourImagesBanner.js
+++ components/FourImagesBanner.js
@@ -2,26 +2,31 @@ import React from "react";
 
 const FourImagesBanner = () => {
   return (
-    <div className="mt-14 w-full mx-auto max-w-7xl flex flex-col justify-between items-center z-10 overflow-hidden py-10">
-      <div className="flex lg:flex-row flex-col justify-between w-full relative px-4">
+    <div className="lg:mt-14 w-full mx-auto max-w-7xl flex flex-col justify-between items-center z-10 overflow-hidden lg:py-10">
+      <div className="flex lg:flex-row flex-col justify-between w-full relative lg:px-4">
         <img
-          className="lg:w-[24%] lg:h-[600px] w-full object-cover lg:block hidden"
+          className="lg:w-[24%] h-[600px] w-full object-cover lg:block hidden"
           src="/IMG/atlantique-breuillet-cambium.jpg"
           alt="résidence"
         />
         <img
-          className="lg:w-[24%] lg:h-[600px] w-full object-cover lg:block "
+          className="lg:w-[24%] h-[600px] w-full object-cover lg:block hidden "
           src="/IMG/atlantique-gujan-lestran.jpg"
           alt="résidence"
         />
         <img
-          className="lg:w-[24%] lg:h-[600px] w-full object-cover lg:block hidden"
+          className="lg:w-[24%] h-[600px] w-full object-cover lg:block hidden"
           src="/IMG/atlantique-lacanau-ocean-swell.jpg"
           alt="résidence"
         />
         <img
-          className="lg:w-[24%] lg:h-[600px] w-full object-cover lg:block hidden"
+          className="lg:w-[24%] h-[600px] w-full object-cover lg:block hidden"
           src="/IMG/atlantique-lacanau-ville-collection.jpg"
+          alt="résidence"
+        />
+        <img
+          className="lg:w-[24%] h-[600px] w-full object-cover lg:hidden block"
+          src="/IMG/progr-cote-atlantique.jpg"
           alt="résidence"
         />
       </div>
@@ -32,11 +37,11 @@ const FourImagesBanner = () => {
         <h2 className="text-blue text-xl lg:text-4xl font-vidaloka">
           JETEZ-VOUS À L’EAU CÔTÉ ATLANTIQUE
         </h2>
-        <p className="text-white text-xl lg:text-2xl">
+        <p className="text-white text-xl lg:text-2xl font-bold">
           plages de sable fin, espaces naturels exceptionnels, centres-villes
           vivants toute l’année...
         </p>
-        <h3 className=" text-xl lg:text-2xl text-blue mt-10">
+        <h3 className=" text-xl lg:text-2xl text-blue mt-10 font-bold">
           LACANAU, CARCANS, GUJAN-MESTRAS, LÈGE CAP-FERRET, ROYAN ATLANTIQUE
         </h3>
         <ul className="text-blue">
