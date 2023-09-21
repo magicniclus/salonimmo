60417cfa1c4fdd3514be22161b58ef471a67b409
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-21 15:05:58 +0200

    Update MerciContainer.js

--- components/MerciContainer.js
+++ components/MerciContainer.js
@@ -1,29 +1,36 @@
 import React from "react";
 
 const MerciContainer = () => {
+  const handleDownload = () => {
+    const link = document.getElementById("hiddenDownloadLink");
+    link.click();
+  };
+
   return (
     <section
       className="min-h-[calc(100vh-150px)] w-full mx-auto max-w-7xl mt-10 lg:mt-0 p-0 sm:p-10 flex justify-center items-center"
-      style={{ zIndex: "100" }}
+      style={{ zIndex: "60" }}
     >
       <div className="min-h-[calc(100vh-150px)] w-full bg-green flex justify-center items-center">
         <div className="p-10 bg-green text-center">
           <h1 className="text-white text-4xl font-light text-center font-vidaloka">
             Merci,
           </h1>
-          <h2 className="text-white text-2xl font-raleway mt-5">
-            Votre message a bien été envoyé.
-          </h2>
-          <p className="text-white text-2xl font-raleway mt-5">
-            Nous vous recontacterons dans les plus brefs délais.
-          </p>
-          <a
-            href="https://www.signature-promotion.fr/"
-            className="text-white text-2xl font-raleway mt-5 inline-block"
-          >
-            Pour retourner à l'accueil,{" "}
-            <span className="text-or font-vidaloka">cliquez ici</span>.
-          </a>
+          <div className="flex flex-col items-center">
+            <button
+              onClick={handleDownload}
+              className="py-5 px-5 bg-blue text-white rounded-full z-100 hover:shadow-xl hover:scale-105 transition-all duration-300 ease-in-out mt-5 w-max"
+            >
+              Télécharger votre invitation
+            </button>
+            <a
+              href="https://www.signature-promotion.fr/"
+              className="text-white text-2xl font-raleway mt-5 inline-block"
+            >
+              Pour retourner à l'accueil,{" "}
+              <span className="text-or font-vidaloka">cliquez ici</span>.
+            </a>
+          </div>
         </div>
       </div>
     </section>
