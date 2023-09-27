76a328b4cb5208e80ff90e913bfc45a904c9c9cc
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-27 12:11:39 +0200

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -41,7 +41,7 @@ const Hero = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const userId = generateUniqueId();
-    writeUserData(userId, lastname, firstname, email, phone, gender)
+    writeCRMUserData(userId, lastname, firstname, email, phone, gender)
       .then((success) => {
         console.log(success);
         router.push("/merci");
